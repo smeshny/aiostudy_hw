639878975fd11ec86daf75ccc23342c4d141a0d0
SIARHEI PALISHCHUK <smeshny@gmail.com>
2024-11-22 01:37:34 +0530

    Working version for T10_3_uniswap_pool_websocket_monitor

--- T10_3_uniswap_pool_websocket_monitor/main.py
+++ T10_3_uniswap_pool_websocket_monitor/main.py
@@ -26,10 +26,15 @@ from modules.websockets.uniswap_monitor import UniswapMonitor
 async def main() -> None:
     """
     Uniswap WebSocket monitor for pool events
+    You can choose any events to listen in UniswapV3Pool contract
+    Most popular events: Swap, Burn, Mint, Flash, Collect, Initialize, CollectProtocol
     """
     
-    UNISWAP_V3_POOL_ADDRESS = '0xC6962004f452bE9203591991D15f6b388e09E8D0' # ETH/USDC V3 pool
-    EVENTS_TO_LISTEN = ['Swap', 'Burn', 'Mint'] # Events to listen. Double check events names in the contract
+    
+    UNISWAP_V3_POOL_ADDRESS = '0xC6962004f452bE9203591991D15f6b388e09E8D0' # Arbitrum ETH/USDC V3 pool
+    # Events to listen:
+    EVENTS_TO_LISTEN = ['Swap', 'Burn', 'Mint', 'Collect', 'Initialize', 'CollectProtocol'] 
+
     
     client = Client(
         account_name="aiostudy", 
